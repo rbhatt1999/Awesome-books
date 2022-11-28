4289a2adc87624becff5be603135637729c5dc75
rbhatt199924 <rbhatt199924@gmail.com>
2022-11-28 18:15:43 +0530

    add script.js file, initiate variable for queryselectors

---
+++ script.js
@@ -0,0 +1,5 @@
+const bookList = document.querySelector('.book-list');
+const submit = document.querySelector('#submit');
+const errorMsg = document.querySelector('.error-msg');
+const formInput = document.querySelectorAll('form input');
+books = [];
