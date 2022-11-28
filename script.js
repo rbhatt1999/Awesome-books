c8d33840b5b4a28199cdf7b2d592a53b37cba9a2
rbhatt199924 <rbhatt199924@gmail.com>
2022-11-28 18:28:38 +0530

    add js code for display function

--- script.js
+++ script.js
@@ -4,4 +4,57 @@ const errorMsg = document.querySelector('.error-msg');
 const formInput = document.querySelectorAll('form input');
 books = [];
 
-submit.addEventListener('click', (event)=>{ event.preventDefault(); let title = formInput[0].value; let author = formInput[1].value; formInput[0].value = ''; formInput[1].value = ''; let id = Date.now(); if(title ==''|| author ==''){ errorMsg.innerHTML= "Please fill the details"; } else { books.push({title,author,id}); localStorage.setItem('books', JSON.stringify(books)); errorMsg.innerHTML=''; display(title,author,id); } }); if(localStorage.getItem('books')){ books = JSON.parse(localStorage.getItem('books')); books.forEach(item => { display(item.title,item.author,item.id); }); } 
+function display (title, author, id){
+  let li = document.createElement('li');
+    li.style.listStyle = 'none';
+    li.id=id;
+    li.innerHTML = `<p>${title}</p>
+    <p>${author}</p>
+    <button class="remove ${id}">Remove</button><hr>`;
+    bookList.appendChild(li);
+    const remove = document.querySelectorAll('.remove');
+    remove.forEach((item)=>{
+      item.addEventListener('click',()=>{
+        books = books.filter((book)=>{
+          if(book.id != item.classList[1]){
+            return true;
+          }
+          else {
+            return false;
+          }
+        })
+        localStorage.setItem('books', JSON.stringify(books));
+        const liList = document.querySelectorAll('.book-list li');
+        liList.forEach((item1)=>{
+          if(item1.id == item.classList[1]){
+            item1.remove();
+          }
+        }) 
+      })
+    })
+}
+
+submit.addEventListener('click', (event)=>{
+  event.preventDefault();
+  let title = formInput[0].value;
+  let author = formInput[1].value;
+  formInput[0].value = '';
+  formInput[1].value = '';
+  let id = Date.now();
+  if(title ==''|| author ==''){
+    errorMsg.innerHTML= "Please fill the details";
+  }
+  else {
+    books.push({title,author,id});
+    localStorage.setItem('books', JSON.stringify(books));
+    errorMsg.innerHTML='';
+    display(title,author,id); 
+  }
+});
+
+if(localStorage.getItem('books')){
+  books = JSON.parse(localStorage.getItem('books'));
+  books.forEach(item => {
+    display(item.title,item.author,item.id);
+  });
+}
