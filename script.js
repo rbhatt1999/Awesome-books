6882ba284484cbab0dd54870e85bfa89e64fd253
Daniel-Kigozi <69048579+Daniel-Kigozi@users.noreply.github.com>
2022-11-28 15:52:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ const submit = document.querySelector('#submit');
 const errorMsg = document.querySelector('.error-msg');
 const formInput = document.querySelectorAll('form input');
 books = [];
+
+submit.addEventListener('click', (event)=>{ event.preventDefault(); let title = formInput[0].value; let author = formInput[1].value; formInput[0].value = ''; formInput[1].value = ''; let id = Date.now(); if(title ==''|| author ==''){ errorMsg.innerHTML= "Please fill the details"; } else { books.push({title,author,id}); localStorage.setItem('books', JSON.stringify(books)); errorMsg.innerHTML=''; display(title,author,id); } }); if(localStorage.getItem('books')){ books = JSON.parse(localStorage.getItem('books')); books.forEach(item => { display(item.title,item.author,item.id); }); } 
