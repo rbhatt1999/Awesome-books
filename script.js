87e998c3643efec5b3144b9cb905188a3ffb8eeb
rbhatt199924 <rbhatt199924@gmail.com>
2022-11-28 18:37:24 +0530

    fix linter errors

--- script.js
+++ script.js
@@ -2,59 +2,56 @@ const bookList = document.querySelector('.book-list');
 const submit = document.querySelector('#submit');
 const errorMsg = document.querySelector('.error-msg');
 const formInput = document.querySelectorAll('form input');
-books = [];
+let books = [];
 
-function display (title, author, id){
-  let li = document.createElement('li');
-    li.style.listStyle = 'none';
-    li.id=id;
-    li.innerHTML = `<p>${title}</p>
+function display(title, author, id) {
+  const li = document.createElement('li');
+  li.id = id;
+  li.innerHTML = `<p>${title}</p>
     <p>${author}</p>
     <button class="remove ${id}">Remove</button><hr>`;
-    bookList.appendChild(li);
-    const remove = document.querySelectorAll('.remove');
-    remove.forEach((item)=>{
-      item.addEventListener('click',()=>{
-        books = books.filter((book)=>{
-          if(book.id != item.classList[1]){
-            return true;
-          }
-          else {
-            return false;
-          }
-        })
-        localStorage.setItem('books', JSON.stringify(books));
-        const liList = document.querySelectorAll('.book-list li');
-        liList.forEach((item1)=>{
-          if(item1.id == item.classList[1]){
-            item1.remove();
-          }
-        }) 
-      })
-    })
+  bookList.appendChild(li);
+  const remove = document.querySelectorAll('.remove');
+  remove.forEach((item) => {
+    item.addEventListener('click', () => {
+      books = books.filter((book) => {
+        if (book.id !== item.classList[1]) {
+          return true;
+        }
+
+        return false;
+      });
+      localStorage.setItem('books', JSON.stringify(books));
+      const liList = document.querySelectorAll('.book-list li');
+      liList.forEach((item1) => {
+        if (item1.id === item.classList[1]) {
+          item1.remove();
+        }
+      });
+    });
+  });
 }
 
-submit.addEventListener('click', (event)=>{
+submit.addEventListener('click', (event) => {
   event.preventDefault();
-  let title = formInput[0].value;
-  let author = formInput[1].value;
+  const title = formInput[0].value;
+  const author = formInput[1].value;
   formInput[0].value = '';
   formInput[1].value = '';
-  let id = Date.now();
-  if(title ==''|| author ==''){
-    errorMsg.innerHTML= "Please fill the details";
-  }
-  else {
-    books.push({title,author,id});
+  const id = Date.now();
+  if (title === '' || author === '') {
+    errorMsg.innerHTML = 'Please fill the details';
+  } else {
+    books.push({ title, author, id });
     localStorage.setItem('books', JSON.stringify(books));
-    errorMsg.innerHTML='';
-    display(title,author,id); 
+    errorMsg.innerHTML = '';
+    display(title, author, id);
   }
 });
 
-if(localStorage.getItem('books')){
+if (localStorage.getItem('books')) {
   books = JSON.parse(localStorage.getItem('books'));
-  books.forEach(item => {
-    display(item.title,item.author,item.id);
+  books.forEach((item) => {
+    display(item.title, item.author, item.id);
   });
 }
