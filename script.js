35b2a13a8460628d03988ece703409a14b45e2c6
rbhatt199924 <rbhatt199924@gmail.com>
2022-11-29 15:51:42 +0530

    replace object and function with class

--- script.js
+++ script.js
@@ -2,36 +2,56 @@ const bookList = document.querySelector('.book-list');
 const submit = document.querySelector('#submit');
 const errorMsg = document.querySelector('.error-msg');
 const formInput = document.querySelectorAll('form input');
-let books = [];
 
-function display(title, author, id) {
-  const li = document.createElement('li');
-  li.id = id;
-  li.innerHTML = `<p>${title}</p>
-    <p>${author}</p>
-    <button class="remove ${id}">Remove</button><hr>`;
-  bookList.appendChild(li);
-  const remove = document.querySelectorAll('.remove');
-  remove.forEach((item) => {
-    item.addEventListener('click', () => {
-      books = books.filter((book) => {
-        if (book.id !== JSON.parse(item.classList[1])) {
-          return true;
-        }
+class booksList {
+  constructor(){
+    this.books = JSON.parse(localStorage.getItem('books')) || [];
+  }
+  addBook(title,author,id){
+    this.title = title;
+    this.author = author;
+    this.id = id;
+    this.books.push({title,author,id});
+    localStorage.setItem('books', JSON.stringify(this.books));
+    this.display(this.title, this.author, this.id);    
+  }
 
-        return false;
-      });
-      localStorage.setItem('books', JSON.stringify(books));
-      const liList = document.querySelectorAll('.book-list li');
-      liList.forEach((item1) => {
-        if (item1.id === item.classList[1]) {
-          item1.remove();
-        }
+  display(title,author,id){
+    const li = document.createElement('li');
+    li.id = id;
+    li.innerHTML = `<p>${title}</p>
+      <p>${author}</p>
+      <button class="remove ${id}">Remove</button><hr>`;
+    bookList.appendChild(li);
+    this.remove();
+  }
+
+  remove(){
+    const remove = document.querySelectorAll('.remove');
+    remove.forEach((item) => {
+      item.addEventListener('click', () => {
+        this.books = this.books.filter((book) => {
+          if (book.id !== JSON.parse(item.classList[1])) {
+            return true;
+          }
+  
+          return false;
+        });
+        localStorage.setItem('books', JSON.stringify(this.books));
+        const liList = document.querySelectorAll('.book-list li');
+        liList.forEach((item1) => {
+          if (item1.id === item.classList[1]) {
+            item1.remove();
+          }
+        });
       });
     });
-  });
+  }
+  
+
 }
 
+let books = new booksList();
 submit.addEventListener('click', (event) => {
   event.preventDefault();
   const title = formInput[0].value;
@@ -42,16 +62,13 @@ submit.addEventListener('click', (event) => {
   if (title === '' || author === '') {
     errorMsg.innerHTML = 'Please fill the details';
   } else {
-    books.push({ title, author, id });
-    localStorage.setItem('books', JSON.stringify(books));
+    books.addBook(title, author, id);
     errorMsg.innerHTML = '';
-    display(title, author, id);
   }
 });
 
 if (localStorage.getItem('books')) {
-  books = JSON.parse(localStorage.getItem('books'));
-  books.forEach((item) => {
-    display(item.title, item.author, item.id);
+  JSON.parse(localStorage.getItem('books')).forEach((item) => {
+    books.display(item.title, item.author, item.id);
   });
 }
