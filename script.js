768bd7d5d5e31a9e2347dd838cceed4d76f57bd5
rbhatt199924 <rbhatt199924@gmail.com>
2022-11-29 19:58:01 +0530

    fix linter issue

--- script.js
+++ script.js
@@ -3,24 +3,25 @@ const submit = document.querySelector('#submit');
 const errorMsg = document.querySelector('.error-msg');
 const formInput = document.querySelectorAll('form input');
 
-class booksList {
-  constructor(){
+class BooksList {
+  constructor() {
     this.books = JSON.parse(localStorage.getItem('books')) || [];
   }
-  addBook(title,author,id){
-    if(this.books){
+
+  addBook(title, author, id) {
+    if (this.books) {
       bookList.classList.add('black-border');
     }
     this.title = title;
     this.author = author;
     this.id = id;
-    this.books.push({title,author,id});
+    this.books.push({ title, author, id });
     localStorage.setItem('books', JSON.stringify(this.books));
-    this.display(this.title, this.author, this.id);    
+    this.display(this.title, this.author, this.id);
   }
 
-  display(title,author,id){
-    if(this.books){
+  display(title, author, id) {
+    if (this.books) {
       bookList.classList.add('black-border');
     }
     const li = document.createElement('li');
@@ -31,7 +32,7 @@ class booksList {
     this.remove();
   }
 
-  remove(){
+  remove() {
     const remove = document.querySelectorAll('.remove');
     remove.forEach((item) => {
       item.addEventListener('click', () => {
@@ -39,11 +40,11 @@ class booksList {
           if (book.id !== JSON.parse(item.classList[1])) {
             return true;
           }
-  
+
           return false;
         });
         localStorage.setItem('books', JSON.stringify(this.books));
-        if(this.books.length == 0){
+        if (this.books.length === 0) {
           bookList.classList.remove('black-border');
         }
         const liList = document.querySelectorAll('.book-list li');
@@ -55,11 +56,9 @@ class booksList {
       });
     });
   }
-  
-
 }
 
-let books = new booksList();
+const books = new BooksList();
 submit.addEventListener('click', (event) => {
   event.preventDefault();
   const title = formInput[0].value;
@@ -68,8 +67,10 @@ submit.addEventListener('click', (event) => {
   formInput[1].value = '';
   const id = Date.now();
   if (title === '' || author === '') {
+    errorMsg.classList.remove('d-hide');
     errorMsg.innerHTML = 'Please fill the details';
   } else {
+    errorMsg.classList.add('d-hide');
     books.addBook(title, author, id);
     errorMsg.innerHTML = '';
   }
